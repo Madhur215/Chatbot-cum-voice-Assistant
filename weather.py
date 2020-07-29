9cf99c15dcf6aaa41842f8277851d19f2b3ac038
rishabhtyagi2306 <rishabhtyagi.2306@gmail.com>
2020-07-29 15:09:08 +0530

    updated intents.json

--- weather.py
+++ weather.py
@@ -1,8 +1,10 @@
 import requests
+import keys
 
 
 def get_weather(city):
-    weather_key = "Enter your key here"
+    key = keys.weather_key()
+    weather_key = key
     url = "https://api.openweathermap.org/data/2.5/weather"
     params = {'APPID': weather_key, 'q': city, 'units': 'imperial'}
     response = requests.get(url, params=params)
